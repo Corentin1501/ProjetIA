f83cfd396516168a20e29d7420a49a9c352554f5
billy <finniss25@gmail.com>
2023-09-18 23:39:16 +0200

    Patient and Rules

--- src/rules.java
+++ src/rules.java
@@ -13,7 +13,7 @@ public class rules {
     public boolean isAnxiete(List<symptome> s){
 
         for(int i=0; i < s.size(); i++) {
-            
+
             if(s.get(i).getNomSymptome() == "INQUIETUDE" && s.get(i).getNomSymptome() == "TROUBLE_MUSCULAIRES" &&
                s.get(i).getNomSymptome() == "TROUBLE_SOMMEIL" && s.get(i).getNomSymptome() == "TROUBLE_CARDIAQUE" &&
                s.get(i).getNomSymptome() == "SUEURS" &&  s.get(i).getNomSymptome() == "CRISE_DE_PANIQUE" )
@@ -26,5 +26,20 @@ public class rules {
         
     }
     
+    public boolean isDepressif(List<symptome> s){
+
+        for(int i=0; i < s.size(); i++) {
+            
+            if(s.get(i).getNomSymptome() == "TRISTESSE" && s.get(i).getNomSymptome() == "PERTE_INTERET" &&
+               s.get(i).getNomSymptome() == "FATIGUE_SEVERE" && s.get(i).getNomSymptome() == "PERTE_APPETIT" &&
+               s.get(i).getNomSymptome() == "TROUBLE_SOMMEIL" &&  s.get(i).getNomSymptome() == "PENSEES_SUICIDAIRES" )
+
+                      return true;
+            
+        }
+        return false;
+
+        
+    }
 
 }
