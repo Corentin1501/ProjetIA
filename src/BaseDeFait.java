73b60ca7e8790d71f3224ab3af45265a50c5c132
corentin <corentin.cuvelier49@gmail.com>
2023-09-18 14:41:37 +0200

    add basedefait

---
+++ src/BaseDeFait.java
@@ -0,0 +1,10 @@
+public class BaseDeFait {
+    
+    String faits[];
+
+    public BaseDeFait(String symptomes[]) {
+        for (int i = 0; i < symptomes.length; i++) {
+            faits[i] = symptomes[i];
+        }
+    }
+}
