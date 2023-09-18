52d04b5a8c9b8d9d84fd81f9296f96f230072847
billy <finniss25@gmail.com>
2023-09-18 19:29:01 +0200

    rules

---
+++ src/maladie.java
@@ -0,0 +1,28 @@
+enum maladies {
+    ANXIETE,
+    DEPRESSION,
+    BIPOLAIRE,
+    SCHIZOPHRENIE,
+    TOC,
+    MIGRAINE,
+    PARKINSON,
+    SCLEROSE_EN_PLAQUES,
+    EPILEPSIE,
+    ALZHEIMER,
+    AUTISME,
+    TDAH
+}
+
+public class maladie {
+
+       
+    String _nom;
+   
+
+    public maladie(String nom) {
+        _nom = nom;
+    }
+    
+
+
+}
---
+++ src/rules.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class rules {
+
+    private List<symptome> symptomesRequis;
+    private List<maladie> maladiesConstates;
+    
+    public rules(List<symptome> sympt, List<maladie> mal) {
+        this.symptomesRequis = sympt;
+        this.maladiesConstates = mal;
+    }
+    
+
+}
